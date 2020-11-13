2419314465065b4e126f9ec1a4150c5c44c7aae0
rogerss25 <romeos25@localhost.localdomain>
2020-11-13 10:13:13 +0700

    fix loop

--- gen_num.py
+++ gen_num.py
@@ -2,17 +2,15 @@
 
 import sys
 
-file = open("number.lst", "w+")
-num = int(sys.argv[1])
+name = str(sys.argv[1])
+file = open(name, 'w+')
+num = int(sys.argv[2])
 x = 0
 
 for x in range(num):
-    """if (x < 10):
-        x = '$0'+str(x)+'\n'
-    elif (x == num-1):
-        x = '$'+str(x)
-    else:
-        x = '$'+str(x)+'\n'"""
+
+    if (x > num):
+        break
     x = str(x)+'\n'
     file.write(x)
 
